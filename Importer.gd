0f269975e5ab071710533900d74da8d5d3447874
Joseph DeVictoria <jldevictoria@gmail.com>
2025-01-31 20:53:22 -0800

    Making more changes towards getting this to run in Godot 4.2

--- Importer.gd
+++ Importer.gd
@@ -1,26 +1,29 @@
 @tool
 extends EditorImportPlugin
 
-func get_importer_name():
+func _get_importer_name():
 	return "lithtech.dtx.import"
 
-func get_visible_name():
+func _get_visible_name():
 	return "Lithtech DTX Importer"
 
-func get_recognized_extensions():
+func _get_recognized_extensions():
 	return ["dtx", "DTX"]
 
-func get_save_extension():
+func _get_save_extension():
 	return "tres"
 
-func get_resource_type():
+func _get_resource_type():
 	return "ImageTexture"
 
-func get_preset_count():
+func _get_preset_count():
 	return 1
 
-func get_preset_name(i):
+func _get_preset_name(i):
 	return "Default"
+
+func _get_priority():
+	return 1
 
 func get_import_options(i):
 	return []
@@ -31,14 +34,11 @@ func get_option_visibility(option, options):
 var _texture_builder = null
 
 func _init():
-	var path = "%s/TextureBuilder.gd" % self.get_script().get_path().get_base_dir()
-	self._texture_builder = load(path).new()
-	
-func import(source_file, save_path, options, platform_variants, gen_files):
+	self._texture_builder = preload("res://addons/godot-dtx-reader/TextureBuilder.gd").new()
+
+func _import(source_file, save_path, options, platform_variants, gen_files):
 	var scene = self._texture_builder.build(source_file, options)
-	
-	var filename = save_path + "." + get_save_extension()
+	var filename = save_path + "." + _get_save_extension()
 	print("Saving as ", filename)
 	ResourceSaver.save(filename, scene)
 	return OK
-
--- LTDtxReader.gd
+++ LTDtxReader.gd
@@ -4,10 +4,8 @@ extends EditorPlugin
 var import_plugin
 
 func _enter_tree():
-	var path = "%s/Importer.gd" % self.get_script().get_path().get_base_dir()
-	import_plugin = load(path).new()
+	import_plugin = preload("res://addons/godot-dat-reader/Importer.gd").new()
 	add_import_plugin(import_plugin)
-
 
 func _exit_tree():
 	remove_import_plugin(import_plugin)
